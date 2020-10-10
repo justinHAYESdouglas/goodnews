9141e11da3b98fae79e246a069d51385931ace0c
Justin Hayes Douglas <justin.hayes.douglas@gmail.com>
2020-10-09 20:33:05 -0500

    Updates

---
+++ precache-manifest.0eae459b68c8d5390fb5ed81bad1ad32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb99d3ed091754797070237f83ed68e6",
+    "url": "/goodnews/index.html"
+  },
+  {
+    "revision": "f090a7ad95eb90ce5444",
+    "url": "/goodnews/static/css/main.63f2c9e3.chunk.css"
+  },
+  {
+    "revision": "8a67442609213732fd73",
+    "url": "/goodnews/static/js/2.79691164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goodnews/static/js/2.79691164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f090a7ad95eb90ce5444",
+    "url": "/goodnews/static/js/main.1a429b0d.chunk.js"
+  },
+  {
+    "revision": "135ee4c7943c653d5a6f",
+    "url": "/goodnews/static/js/runtime-main.f7a9d8f8.js"
+  },
+  {
+    "revision": "3066b11123e63381b8e2aa554b18bf32",
+    "url": "/goodnews/static/media/bauhaus-93-regular.3066b111.ttf"
+  }
+]);
